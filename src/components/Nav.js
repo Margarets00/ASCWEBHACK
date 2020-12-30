7ccc1a0977fe04bc06891582b25a44b23b1b6c8a
Margarets00 <minj01134@gmail.com>
2020-12-30 19:05:45 +0900

    second problem

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,30 @@
 
-function Nav({number,value,setPage}) {
+import React,{useState,useEffect} from 'react';
+
+function Nav({number,setPage}) {
+    const answers = [
+        {solve:'어떠한',code:'password'},
+        {solve:'pp33pp44',code:'happynewyear'},
+        {solve:'오늘저녁은치킨이닭',code:'happynewyear'},
+        {solve:'새해복많이받으세요',code:'happynewyear'},
+    ]
+    const [value,setValue]=useState('');
+    const [correct,setCorrect]=useState(false);
+    useEffect(() => {
+        setValue('');
+        setCorrect(false);
+    }, [number])
+
     function nextPage(e) {
         setPage(e);
+        setValue('');
+        setCorrect(false);
     }
     function checkAnswer(value) {
-        if(value==='\'어떠한\' 비밀번호'){
-            setPage(1);
+        if(value===answers[number].solve){
+            setCorrect(true);
         }
+
     }
     return (
         <div className="Nav flex row">
@@ -17,13 +35,30 @@ function Nav({number,value,setPage}) {
           >
             Back
           </a>
+          <div className="flex row input-form">
+            {!correct?
+                <input type="text" value={value} onChange={(e)=>setValue(e.target.value)} placeholder="put answer here"/>
+              :
+              <p className="App-link">Corrected! The Code is {answers[number].code}</p>
+            }
+        </div>
+        {!correct?
           <a
             className="App-link"
             onClick={()=>checkAnswer(value)}
             rel="noopener noreferrer"
           >
+            Okay
+          </a>
+          :
+          <a
+            className="App-link"
+            onClick={()=>nextPage(1)}
+            rel="noopener noreferrer"
+          >
             Next
           </a>
+        }
         </div>
     )
 }
--- src/components/Problem.js
+++ src/components/Problem.js
@@ -1,24 +1,19 @@
 import Nav from './Nav';
-import React,{useState,useEffect} from 'react';
 const problems = [
-    {header:'비밀번호를 알아내자',contents:'ASCWEBHACK의 주최자 정진우는 평소 개인정보보호에 관심이 많은 편입니다. 이번 대회의 첫번째 입장 비밀번호를 정하기 위해 많은 고민을 한 결과, 대회 입장 비밀번호를 어떠한 ‘비밀번호’로 설정하였습니다. 개인정보보호에 관심이 많은 진우는 과연 어떤 비밀번호를 고안하였을까요?'},
-    {header:'',contents:''},
+    {header:'비밀번호를 알아내자',hint:'정답은 언제나 문제 속에 있습니다',contents:'ASCWEBHACK의 주최자 정진우는 평소 개인정보보호에 관심이 많은 편입니다. 이번 대회의 첫번째 입장 비밀번호를 정하기 위해 많은 고민을 한 결과, 대회 입장 비밀번호를 어떠한 ‘비밀번호’로 설정하였습니다. 개인정보보호에 관심이 많은 진우는 과연 어떤 비밀번호를 고안하였을까요?'},
+    {header:'/*Password*/',hint:'F12를 눌러보세요',contents:'기억력이 나쁜 블랑카는 언제나 주석으로 비밀번호를 적어두는 습관이 있습니다...'},
     {header:'',contents:''},
     {header:'',contents:''},
 ]
 
 function Problems({number,setPage}){
-    const [value,setValue]=useState('');
 
-    useEffect(() => {
-        setValue('');
-    }, [number])
 
     return (
         <>
         <div className="header baseline flex column">
         <p>
-          Problem{number}. {problems[number].header}
+          Problem {number}. {problems[number].header}
         </p>
         </div>
         <div className="contents flex flex-center">
@@ -26,10 +21,12 @@ function Problems({number,setPage}){
             {problems[number].contents}
         </p>
         </div>
-        <div className="input-form">
-            <input type="text" value={value} onChange={(e)=>setValue(e.target.value)} placeholder="put answer here"/>
+        <div className="hint">
+            <p>Hint?</p>
+            <span>{problems[number].hint}</span>
         </div>
-        <Nav numbers={number} value={value} setPage={setPage}/>
+        
+        <Nav number={number} setPage={setPage}/>
         </>
     )
 
